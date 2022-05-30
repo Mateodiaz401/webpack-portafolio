5bb57b0752d806917668079af280a0883ab14f62
Mateo Díaz <mateodiaz401@gmail.com>
2022-05-30 18:15:25 -0500

    el primer commit

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("Hello"),console.log(5+8)})();
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import sum from './utils/sum.js'
+
+const hello = "Hello";
+
+console.log(hello);
+
+console.log(sum( 5, 8));
