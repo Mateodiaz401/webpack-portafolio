b084abfebfb490d738b0cd6c516f37841e98052d
Mateo Díaz <mateodiaz401@gmail.com>
2022-05-30 23:40:02 -0500

    Se elimino el watch para la configuracion de desarrollo

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -14,7 +14,7 @@ module.exports = {
         assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     mode: 'development',
-    watch: true,
+    // watch: true,
     resolve: {
         extensions: ['.js'],
         alias: {
